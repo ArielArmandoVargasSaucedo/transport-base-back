ecb2b481de0f4e4e95a44c031507c3170f9b711c
ArielVS <146688229+ArielArmandoVargasSaucedo@users.noreply.github.com>
2024-08-14 22:33:50 -0400

    Actualización back autenticación

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -1,4 +1,12 @@
-import { Controller } from '@nestjs/common';
+import { Body, Controller, Post, Query} from '@nestjs/common';
+import { AuthService } from './auth.service';
 
 @Controller('auth')
-export class AuthController {}
+export class AuthController {
+  constructor(private readonly authService: AuthService) {}
+
+  @Post('login')
+  public async login(@Query('user_name') user_name: string, @Query('password_user') password_user: string) {
+    return  await this.authService.login(user_name, password_user)
+  } 
+}
