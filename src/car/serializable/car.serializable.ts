10fc1aba7e6e64a37cfde6dec95b1b02620fa71b
Adrian <adrian.suarez.alphen@gmail.com>
2024-08-16 16:28:45 -0400

    Error en la serialización de los carros, corregido

--- src/car/serializable/car.serializable.ts
+++ src/car/serializable/car.serializable.ts
@@ -12,6 +12,7 @@ export class CarSerializable {
         car_brand: string,
         number_of_seats: number,
         car_situation: CarSitutationSerializable) {
+        this.id_car = id_car
         this.car_number = car_number
         this.car_brand = car_brand
         this.number_of_seats = number_of_seats
